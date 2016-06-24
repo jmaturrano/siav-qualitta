e68355fc19436773ffa277cfd8ac7dce696cd0d9
Julio Maturrano <julio16lb@gmail.com>
2016-06-24 04:00:35 -0500

    ajustes de perfil de usuario

--- application/controllers/perfil/Usuario.php
+++ application/controllers/perfil/Usuario.php
@@ -1,36 +1,17 @@
 <?php
-
-
-
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
-
-
-
 class Usuario extends CI_Controller {
-
     private static $header_title  = 'Perfil Usuario';
-
     private static $header_icon  = ICON_SETTINGS;
-
     private static $OFICINAS = array();
     private static $ROLES = array();
     private static $PRIVILEGIOS = array();
-
-
     public function __construct() {
-
         parent :: __construct();
-
         $this->initData();
-        
         $this->load->model('seguridad/usuario_model');
-
         $this->load->model('seguridad/docidentidad_model');
-
-        
-
     }
 
     public function initData(){
@@ -39,110 +20,53 @@ class Usuario extends CI_Controller {
         self::$PRIVILEGIOS  = revisar_privilegios($this, 0);    
     }
 
-
-
     public function index() {
-        
-
+        redirect('/');
     }
-
-
 
     public function editar($usua_id_enc = ''){
         $data['OFICINAS']       = self::$OFICINAS;
         $data['ROLES']          = self::$ROLES;
         $data['PRIVILEGIOS']    = self::$PRIVILEGIOS;
-
         ($usua_id_enc === '') ? redirect('seguridad/usuario') : '';
-
         $usua_id = str_decrypt($usua_id_enc, KEY_ENCRYPT);
-
         $this->load->library('layout');
-
         $data['header_title']   = self::$header_title;
-
         $data['header_icon']    = self::$header_icon;
-
         $data['tipo_vista']     = 'editar';
-
         $data['data_usua']      = $this->usuario_model->getUsuarioByID($usua_id);
-
         $data['data_doid']      = $this->docidentidad_model->getDocidentidadAll();
-
-        
-
         $data['btn_guardar']    = true;
-
         $data['btn_cancelar']   = 'seguridad/usuario';
-
         $this->layout->view('perfil/perfil_form', $data);
-
         $this->load->view('notificacion');
 
-
-
     }
-
-
-
 
     public function guardar($usua_id_enc = ''){
         $data['OFICINAS']       = self::$OFICINAS;
         $data['ROLES']          = self::$ROLES;
         $data['PRIVILEGIOS']    = self::$PRIVILEGIOS;
 
-        $usua_id = ($usua_id_enc === '') ? '' : str_decrypt($usua_id_enc, KEY_ENCRYPT);
-
-        //$this->form_validation->set_rules('usua_nombre', 'Nombre Usuario', 'required');
-
-        //$this->form_validation->set_rules('dide_id', 'Tipo Documento', 'required');
-
-        //$this->form_validation->set_rules('usua_numero_documento', 'Doc. Identidad', 'required');
-        
+        $usua_id = ($usua_id_enc === '') ? '' : str_decrypt($usua_id_enc, KEY_ENCRYPT);        
         if($usua_id !== ''){
-
             $this->form_validation->set_rules('usua_clave', 'Clave', 'required');
-
         }
-        
-        //$this->form_validation->set_rules('usua_fecha_expiracion', 'Fecha de Expiración', 'required');
-
         $data['header_title']   = self::$header_title;
-
         $data['header_icon']    = self::$header_icon;
-
         $data['tipo_vista']     = ($usua_id === '')?'nuevo':'editar';
-
         $data['data_usua']      = $this->usuario_model->getUsuarioByID($usua_id);
-
         $data['data_doid']      = $this->docidentidad_model->getDocidentidadAll();
-
         $data['btn_guardar']    = true;
-
         $data['btn_cancelar']   = 'seguridad/usuario';
-
         $this->load->library('layout');
-
-       
-
-        
-
-
-            $datapost = $this->security->xss_clean($this->input->post());
-
+        $datapost = $this->security->xss_clean($this->input->post());
             //print_r($datapost);
-
             if($datapost['usua_clave'] != ''){
-
                 $data_usua = array(
-
-                    
                     'usua_clave'              => md5($datapost['usua_clave'])
-
                 );
-
             }
-
             if ($this->form_validation->run() === FALSE) {
                 $this->session->set_flashdata('mensaje_tipo', EXIT_ERROR);
                 $this->session->set_flashdata('mensaje', primer_error_validation());
@@ -150,37 +74,19 @@ class Usuario extends CI_Controller {
             }
             else
             {
-
                 $data_response = $this->usuario_model->updateUsuario($data_usua, $usua_id);
-
                 if($data_response){
-
                     $this->session->set_flashdata('mensaje_tipo', EXIT_SUCCESS);
-
                     $this->session->set_flashdata('mensaje', RMESSAGE_UPDATE);
-
                     redirect('perfil/usuario/editar/'.str_encrypt($usua_id, KEY_ENCRYPT), $data);
-
                 }else{
-
                     $this->session->set_flashdata('mensaje_tipo', EXIT_ERROR);
-
                     $this->session->set_flashdata('mensaje', RMESSAGE_ERROR);
-
                     $this->layout->view('perfil/usuario_form', $data);
-
                 }
-
             }
-
         $this->load->view('notificacion');
-
-
-
-
     }
-
-   
 
 
 }
--- application/views/perfil/perfil_form.php
+++ application/views/perfil/perfil_form.php
@@ -1,169 +1,82 @@
 <div class="main">
-
 	<div class="main-inner">
-
 	    <div class="container">
-
 	      <div class="row">
-
 	      	<div class="span12">
-
 	      		<div class="widget">
-
 	      			<div class="widget-header">
-
 	      				<i class="<?= $header_icon; ?>"></i>
-
 	      				<h3><?= $header_title; ?></h3>
-
 	                    <ul class="breadcrumb custom-breadcrumb">
-
 	                        <li><a href="<?php echo base_url('panel/principal') ?>"><i class="<?php echo ICONO_HOME ?>"></i>&nbsp; Inicio</a></li>
-
 	                        <li>&nbsp; /</li>
-
-	                        
-
 	                        <li class="active"><i class="<?= ICON_FORM; ?>"></i>&nbsp; Formulario</li>
-
 	                    </ul>
-
 	  				</div> <!-- /widget-header -->
-
 					<div class="widget-content">
-
 						<div id="formcontrols" class="">
-
 							<?php
-
 							$attributes = array(
-
 							  'id'      	=> 'form_usuario',
-
 							  'name'    	=> 'form_usuario',
-
 							  'class'		=> 'form-horizontal',
-
 							  'tipo_vista' 	=> $tipo_vista
-
 							  );
-
 							$ruta = 'perfil/usuario/guardar/'.((isset($data_usua))?str_encrypt($data_usua->usua_id, KEY_ENCRYPT):'');
-
 							echo validation_errors();
-
 							echo form_open($ruta, $attributes);
-
 							?>
-
 							<fieldset>
-
 								<div class="control-group">
-
 									<label for="usua_nombre" class="control-label">Nombre</label>
-
 									<div class="controls">
-
 										<input type="text" value="<?= (isset($data_usua))?$data_usua->usua_nombre:set_value('usua_nombre'); ?>" id="usua_nombre" name="usua_nombre" class="span8" maxlength="40" disabled>
-
 									</div> <!-- /controls -->
-
 								</div> <!-- /control-group -->
-
 								<div class="control-group">
-
 									<label for="dide_id" class="control-label">Tipo Documento</label>
-
 									<div class="controls">
-
 					                    <select class="selectpicker span8" name="dide_id" id="dide_id" disabled>
-
 					                    <?php
-
 					                    if(isset($data_doid)){
-
 					                    	foreach ($data_doid as $item => $docidentidad) {
-
 					                   	?>
-
 											<option value="<?= $docidentidad->dide_id; ?>" data-length="<?= $docidentidad->dide_caracteres; ?>" <?= (isset($data_usua) && $data_usua->dide_id === $docidentidad->dide_id)?'selected': set_select('dide_id', $docidentidad->dide_id); ?>><?= $docidentidad->dide_descripcion; ?></option>
-
 					                   	<?php
-
 					                    	}
-
 					                    }
-
 					                    ?>
-
 					                    </select>
-
 									</div> <!-- /controls -->
-
 								</div> <!-- /control-group -->
-
 								<div class="control-group">
-
 									<label for="usua_numero_documento" class="control-label">Doc. Identidad <span id="dideid_maxlength">(8 dig.)</span></label>
-
 									<div class="controls">
-
 										<input type="text" value="<?= (isset($data_usua))?$data_usua->usua_numero_documento:set_value('usua_numero_documento'); ?>" id="usua_numero_documento" name="usua_numero_documento" class="span8" maxlength="8" disabled>
-
 									</div> <!-- /controls -->
-
 								</div> <!-- /control-group -->
-
 								<div class="control-group">
-
 									<label for="usua_clave" class="control-label">Clave</label>
-
 									<div class="controls checkbox input-group">
-
 										<label class="input-group-addon" style="display: none;">
-
 											<input type="checkbox" class="chkbx_x">
-
 										</label>
-
 										<input type="password" value="" id="usua_clave" name="usua_clave" class="span8 form-control" maxlength="50">
-
 									</div> <!-- /controls -->
-
 								</div> <!-- /control-group -->
-
-
 							</fieldset>
-
 					       	<?php
-
 					        echo form_close();
-
 					        ?>
-
 							<hr>
-
 							<br>
-
-
-
-
 						</div>
-
 					</div> <!-- /widget-content -->
-
 				</div> <!-- /widget -->
-
 		    </div> <!-- /span8 -->
-
 	      </div> <!-- /row -->
-
 	    </div> <!-- /container -->
-
 	</div> <!-- /main-inner -->
-
 </div> <!-- /main -->
-
-
 
 <script src="<?php echo base_url('public/assets/js/Usuario.js') ?>"></script>
