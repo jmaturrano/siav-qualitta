7a63b8e615dea17e8cb648779a9279e4d2ea78e8
Gerberth <gcontreras@mksystemsoft.com>
2016-06-28 22:45:09 -0500

    cambios realizados antes del 28-06-2016

---
+++ public/assets/js/Listaprecios.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	if($('#form_certificadoslegales').length > 0){
+		var tipo_vista = ($('#form_certificadoslegales').attr('tipo_vista')).toUpperCase();
+		if(tipo_vista === 'VER'){
+			$('#form_certificadoslegales').find('input').attr('disabled', 'disabled');
+			$('#form_certificadoslegales').find('select').attr('disabled', 'disabled');
+		}
+	}
+
+	$(document).on('click', '.btn_guardar', function(e){
+		$('#form_certificadoslegales').submit();
+		e.preventDefault();
+	});
+
+});
