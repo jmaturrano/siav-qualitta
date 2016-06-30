278f7a23dccba44df41333dfa856a4e0d0ef6bc5
Julio Maturrano <julio16lb@gmail.com>
2016-06-30 01:35:11 -0500

    mant modalidad comentado 100% (controlador, modelo)

--- application/models/registros/Modalidad_model.php
+++ application/models/registros/Modalidad_model.php
@@ -1,155 +1,138 @@
-<?php
-
-
-
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-
-
-class Modalidad_Model extends CI_Model {
-
-    private static $table_menu  = 'modalidad';
-
-
-
-    public function __construct() {
-
-        parent :: __construct();
-
-        $this->load->database();
-
-    }
-
-
-
-   
-
-    public function getModalidadAll($q = '', $limit = 1000, $offset = 0){
-
-        $this->db->limit($limit, $offset);
-
-        $where = array('moda_estado'=>DB_ACTIVO);
-
-        if($q !== ''){
-
-            $this->db->group_start();
-
-            $this->db->like('moda_descripcion',$q);
-
-            $this->db->group_end();
-
-        }
-
-        $query = $this->db->order_by('moda_descripcion', 'asc')->where($where)->get(self::$table_menu);
-
-        if($query->num_rows() > 0){
-
-            return $query->result();
-
-        }
-
-        return null;
-
-    }
-
-
-
-    public function contar_estructuras_todos() {
-
-        $this->db->where('moda_estado', DB_ACTIVO);
-
-        return $this->db->count_all_results(self::$table_menu);
-
-    }
-
-
-
-    public function getModalidadByID($moda_id){
-
-        $where = array('moda_id' => $moda_id);
-
-        $query = $this->db->where($where)->get(self::$table_menu);
-
-        if($query->num_rows() > 0){
-
-            return $query->row();
-
-        }
-
-        return null;
-
-    }
-
-
-
-    public function deleteModalidadByID($moda_id){
-
-        $where      = array('moda_id' => $moda_id);
-
-        $data_modal = array('moda_estado' => DB_INACTIVO);
-
-        $query      = $this->db->trans_begin();
-
-        $query      = $this->db->where($where)->update(self::$table_menu, $data_modal);
-
-        if ($this->db->trans_status() === FALSE){
-
-            $this->db->trans_rollback();
-
-            return false;
-
-        }
-
-        $this->db->trans_commit();
-
-        return true;
-
-    }
-
-
-
-    public function insertModalidad($data_modal){
-
-        $query      = $this->db->trans_begin();
-
-        $query      = $this->db->insert(self::$table_menu, $data_modal);
-
-        if ($this->db->trans_status() === FALSE){
-
-            $this->db->trans_rollback();
-
-            return false;
-
-        }
-
-        $this->db->trans_commit();
-
-        return true;
-
-    }
-
-
-
-    public function updateModalidad($data_modal, $moda_id){
-
-        $where      = array('moda_id' => $moda_id);
-
-        $query      = $this->db->trans_begin();
-
-        $query      = $this->db->where($where)->update(self::$table_menu, $data_modal);
-
-        if ($this->db->trans_status() === FALSE){
-
-            $this->db->trans_rollback();
-
-            return false;
-
-        }
-
-        $this->db->trans_commit();
-
-        return true;
-
-    }
-
+<?php
+/**
+* MK System Soft  
+*
+* Modelo de Modalidad carreras
+*
+*/
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Modalidad_Model extends CI_Model {
+    private static $table_menu  = 'modalidad';
+    public function __construct() {
+        parent :: __construct();
+        $this->load->database();
+    }
+
+    /**
+      * Fx de Lista de Modalidad
+      *
+      * Trae la lista de todos los registros activos en general o por búsqueda específica
+      *
+      * @param string $q parámetro de búsqueda
+      * @param string $limit cantidad límite por búsqueda realizada o listado general
+      * @param string $offset número de página o inicio de búsqueda
+      *
+      * @return void
+      */
+    public function getModalidadAll($q = '', $limit = 1000, $offset = 0){
+        $this->db->limit($limit, $offset);
+        $where = array('moda_estado'=>DB_ACTIVO);
+        if($q !== ''){
+            $this->db->group_start();
+            $this->db->like('moda_descripcion',$q);
+            $this->db->group_end();
+        }
+        $query = $this->db->order_by('moda_descripcion', 'asc')->where($where)->get(self::$table_menu);
+        if($query->num_rows() > 0){
+            return $query->result();
+        }
+        return null;
+    }
+
+    /**
+      * Fx de Contar registros
+      *
+      * Cuenta todos los registros activos de la tabla
+      *
+      *
+      * @return void
+      */
+    public function contar_estructuras_todos() {
+        $this->db->where('moda_estado', DB_ACTIVO);
+        return $this->db->count_all_results(self::$table_menu);
+    }
+
+    /**
+      * Fx de Lista de modalidad por ID
+      *
+      * Devuelve los datos de un registro específico
+      *
+      * @param int $moda_id id principal del registro
+      *
+      * @return void
+      */
+    public function getModalidadByID($moda_id){
+        $where = array('moda_id' => $moda_id);
+        $query = $this->db->where($where)->get(self::$table_menu);
+        if($query->num_rows() > 0){
+            return $query->row();
+        }
+        return null;
+    }
+
+    /**
+      * Fx de Eliminación de registro
+      *
+      * Actualiza el registro a estado inactivo
+      *
+      * @param int $moda_id id principal del registro
+      *
+      * @return void
+      */
+    public function deleteModalidadByID($moda_id){
+        $where      = array('moda_id' => $moda_id);
+        $data_modal = array('moda_estado' => DB_INACTIVO);
+        $query      = $this->db->trans_begin();
+        $query      = $this->db->where($where)->update(self::$table_menu, $data_modal);
+        if ($this->db->trans_status() === FALSE){
+            $this->db->trans_rollback();
+            return false;
+        }
+        $this->db->trans_commit();
+        return true;
+    }
+
+    /**
+      * Fx de Inserción de registro
+      *
+      * Inserta un registro nuevo
+      *
+      * @param array $data_modal contiene los datos a ingresar
+      *
+      * @return void
+      */
+    public function insertModalidad($data_modal){
+        $query      = $this->db->trans_begin();
+        $query      = $this->db->insert(self::$table_menu, $data_modal);
+        if ($this->db->trans_status() === FALSE){
+            $this->db->trans_rollback();
+            return false;
+        }
+        $this->db->trans_commit();
+        return true;
+    }
+
+    /**
+      * Fx de Actualización de registro
+      *
+      * Actualiza un registro activo
+      *
+      * @param array $data_modal contiene los datos a ingresar
+      * @param int $moda_id id principal del registro
+      *
+      * @return void
+      */
+    public function updateModalidad($data_modal, $moda_id){
+        $where      = array('moda_id' => $moda_id);
+        $query      = $this->db->trans_begin();
+        $query      = $this->db->where($where)->update(self::$table_menu, $data_modal);
+        if ($this->db->trans_status() === FALSE){
+            $this->db->trans_rollback();
+            return false;
+        }
+        $this->db->trans_commit();
+        return true;
+    }
+
 }
--- application/views/registros/modalidad_form.php
+++ application/views/registros/modalidad_form.php
@@ -1,99 +1,56 @@
-<div class="main">
-
-	<div class="main-inner">
-
-	    <div class="container">
-
-	      <div class="row">
-
-	      	<div class="span12">
-
-	      		<div class="widget">
-
-	      			<div class="widget-header">
-
-	      				<i class="<?= $header_icon; ?>"></i>
-
-	      				<h3><?= $header_title; ?></h3>
-
-	                    <ul class="breadcrumb custom-breadcrumb">
-
-	                        <li><a href="<?php echo base_url('panel/principal') ?>"><i class="<?php echo ICONO_HOME ?>"></i>&nbsp; Inicio</a></li>
-
-	                        <li>&nbsp; /</li>
-
-	                        <li><a href="<?php echo base_url('registros/modalidad') ?>"><i class="<?= $header_icon; ?>"></i>&nbsp; <?= $header_title; ?></a></li>
-
-	                        <li>&nbsp; /</li>
-
-	                        <li class="active"><i class="<?= ICON_FORM; ?>"></i>&nbsp; Formulario</li>
-
-	                    </ul>
-
-	  				</div> <!-- /widget-header -->
-
-					<div class="widget-content">
-
-						<div id="formcontrols" class="">
-
-							<?php
-
-							$attributes = array(
-
-							  'id'      	=> 'form_modalidad',
-
-							  'name'    	=> 'form_modalidad',
-
-							  'class'		=> 'form-horizontal',
-
-							  'tipo_vista' 	=> $tipo_vista
-
-							  );
-
-							$ruta = 'registros/modalidad/guardar/'.((isset($data_moda))?str_encrypt($data_moda->moda_id, KEY_ENCRYPT):'');
-
-							echo form_open($ruta, $attributes);
-
-							?>
-
-							<fieldset>
-
-								<div class="control-group">
-
-									<label for="moda_descripcion" class="control-label">Descripción</label>
-
-									<div class="controls">
-
-										<input type="text" value="<?= (isset($data_moda))?$data_moda->moda_descripcion:set_value('moda_descripcion'); ?>" id="moda_descripcion" name="moda_descripcion" class="span8" maxlength="100">
-
-									</div> <!-- /controls -->
-
-								</div> <!-- /control-group -->
-
-							</fieldset>
-
-					       	<?php
-
-					        echo form_close();
-
-					        ?>
-
-						</div>
-
-					</div> <!-- /widget-content -->
-
-				</div> <!-- /widget -->
-
-		    </div> <!-- /span8 -->
-
-	      </div> <!-- /row -->
-
-	    </div> <!-- /container -->
-
-	</div> <!-- /main-inner -->
-
-</div> <!-- /main -->
-
-
-
+<div class="main">
+	<div class="main-inner">
+	    <div class="container">
+	      <div class="row">
+	      	<div class="span12">
+	      		<div class="widget">
+	      			<div class="widget-header">
+	      				<i class="<?= $header_icon; ?>"></i>
+	      				<h3><?= $header_title; ?></h3>
+	                    <ul class="breadcrumb custom-breadcrumb">
+	                        <li><a href="<?php echo base_url('panel/principal') ?>"><i class="<?php echo ICONO_HOME ?>"></i>&nbsp; Inicio</a></li>
+	                        <li>&nbsp; /</li>
+	                        <li><a href="<?php echo base_url('registros/modalidad') ?>"><i class="<?= $header_icon; ?>"></i>&nbsp; <?= $header_title; ?></a></li>
+	                        <li>&nbsp; /</li>
+	                        <li class="active"><i class="<?= ICON_FORM; ?>"></i>&nbsp; Formulario</li>
+	                    </ul>
+	  				</div> <!-- /widget-header -->
+					<div class="widget-content">
+						<div id="formcontrols" class="">
+							<?php
+							$attributes = array(
+							  'id'      	=> 'form_modalidad',
+							  'name'    	=> 'form_modalidad',
+							  'class'		=> 'form-horizontal',
+							  'tipo_vista' 	=> $tipo_vista
+							  );
+							$ruta = 'registros/modalidad/guardar/'.((isset($data_moda))?str_encrypt($data_moda->moda_id, KEY_ENCRYPT):'');
+							echo form_open($ruta, $attributes);
+							?>
+							<fieldset>
+								<div class="control-group">
+									<label for="moda_descripcion" class="control-label">Descripción</label>
+									<div class="controls">
+										<input type="text" value="<?= (isset($data_moda))?$data_moda->moda_descripcion:set_value('moda_descripcion'); ?>" id="moda_descripcion" name="moda_descripcion" class="span8" maxlength="100">
+									</div> <!-- /controls -->
+								</div> <!-- /control-group -->
+							</fieldset>
+					       	<?php
+					        echo form_close();
+					        ?>
+						</div>
+					</div> <!-- /widget-content -->
+				</div> <!-- /widget -->
+		    </div> <!-- /span8 -->
+	      </div> <!-- /row -->
+	    </div> <!-- /container -->
+	</div> <!-- /main-inner -->
+</div> <!-- /main -->
+
+
+
+
+
+
+
 <script src="<?php echo base_url('public/assets/js/Modalidad.js') ?>"></script>
